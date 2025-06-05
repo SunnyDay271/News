b7206861af420cd11ec6b0532e0da72ad3f9a76a
Day (Purwa) <thinman27l@gmail.com>
2025-06-05 11:17:01 +0700

    Update NewsDetail.jsx

--- sybau/src/Pages/NewsDetail.jsx
+++ sybau/src/Pages/NewsDetail.jsx
@@ -1,43 +1,31 @@
 import { Container, Row, Col } from "react-bootstrap"
-import {testimonial} from "../data/index"
-import { FaRegSun, FaRegMoon } from "react-icons/fa6";
+import { testimonial } from "../data/index"
 
 import FaqComponent from "../components/FaqComponent"
-import { useState } from "react";
+import { semuaKelas } from "../data/index";
+import { useParams } from "react-router-dom";
+// import { useState } from "react";
 
 const NewsDetail = () => {
-    const [ theme, setTheme ] = useState("")
+  const { id } = useParams()
+  const post = semuaKelas.find((p) => p.id == parseInt(id))
+  const posts = testimonial.find((p) => p.id == parseInt(id))
+  console.log(post)
 
   return (
-  <div className="Detail-page">
-    <div className="Detail">
-      {/* <div 
-    className={`${
-          theme ? "dark" : "" }
-          bg-white dark:bg-zinc-800 grid place-items-center h-screen w-full `}>
-            <div className="bg-zinc-100 dark:bg-zinc-700 pt-18  rounded-xl">
-              <button 
-              onClick={() => {
-                  setTheme("")
-                }}
-                className="bg-transparent dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
-                  <FaRegSun />
-                </button>
-                <button 
-                onClick={() => {
-                    setTheme("dark")
-                  }}
-                  className="bg-transparent dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
-                    <FaRegMoon />
-                  </button> */}
-      <Container>
-        <Row className="mb-5">
-          <Col>
-          <h1 className="fw-bold text-center animate__animated animate__backInUp animate__delay-1s">Semua Detail Berita</h1>
-          <p className="text-center animate__animated animate__backInUp animate__delay-1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, perferendis!</p>
-          </Col>
-        </Row>
-        <Row className="row-cols-lg-3 row-cols-1">
+    <div className="Detail-page">
+      <div className="Detail">
+        <Container>
+          <Row className="mb-5">
+            <Col>
+              <h1 className="fw-bold text-center animate__animated animate__backInUp animate__delay-1s">{post.title}</h1>
+              <p className="text-center animate__animated animate__backInUp animate__delay-1s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, perferendis!</p>
+              <img className="shadow-sm animate__animated animate__backInUp animate__delay-2s" src={post.image} alt="Gambar Karate" />
+              <h2 className="fw-bold animate__animated animate__backInUp animate__delay-2s">{posts.name}</h2>
+              <p className="animate__animated animate__backInUp animate__delay-2s">{posts.desc}</p>
+            </Col>
+          </Row>
+          
           { testimonial.map((data) => {
         return(
           <Col key={data.id} className="mb-5 border border-p-5">
@@ -52,13 +40,10 @@ const NewsDetail = () => {
           </Col>
         )
       })}
-        </Row>
-      </Container>
+        </Container>
+      </div>
+      <FaqComponent />
     </div>
-    <FaqComponent />
-  </div>
-// </div>
-// </div>
   )
 }
 
